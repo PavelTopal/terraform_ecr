2fd6605e75c8474f3acbc0952b1b45f555b277fc
PavelTopal <paveltopal0@gmail.com>
2024-03-30 11:19:01 -0500

    deleted s3 bucket lines

--- main.tf
+++ main.tf
@@ -2,18 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "myapp-repo" {
-  name                 = "myapp-repo"
+resource "aws_ecr_repository" "hello-app" {
+  name                 = "hello-app"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
-  }
-}
-
-terraform {
-  backend "s3" {
-    key    = "tf-ecr"
-    region = "us-east-1"    
   }
 }
